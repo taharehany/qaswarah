093a4e3507532370ddb9bbdf381b7b377d838235
‪Taha Al-rehany‬‏ <32350359+taharehany@users.noreply.github.com>
2021-11-13 23:51:28 +0200

    last......

--- send.php
+++ send.php
@@ -118,7 +118,7 @@
       <main class="main">
          <div class="container">
             <h3> <?php
-                    $email= "contact@qaswarahagency.com";
+                    $email= "qaswarahagency@gmail.com";
                     if (!empty($_POST)){
                         if(empty($_POST['name']) or empty($_POST['phone']) or empty($_POST['email']) or empty($_POST['message'])){
                             echo("تأكد من ملئ جميع الحقول!");
